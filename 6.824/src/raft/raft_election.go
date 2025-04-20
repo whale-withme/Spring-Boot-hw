c3a3ad7f4eb3cac9f99ebff6b5a17e5140ce853d
whale-withme <3052748978@qq.com>
2025-04-20 16:55:39 +0800

    add 6.824

---
+++ 6.824/src/raft/raft_election.go
@@ -0,0 +1,53 @@
+package raft
+
+import "sync/atomic"
+
+// send RequestVote RPC for all other servers.
+// record response votes and check if get majority votes
+// change state to leader/follower.
+func (rf *Raft) startElection() {
+	request := RequestVoteArgs{
+		Term:        rf.currentTerm,
+		CandidateId: rf.me,
+	}
+
+	var grantedVotes int32
+	grantedVotes = 1
+	rf.voteFor = rf.me
+
+	for serverId, _ := range rf.peers {
+		if serverId == rf.me {
+			continue
+		}
+
+		go func(id int) {
+			response := RequestVoteReply{}
+			if rf.sendRequestVote(serverId, &request, &response) {
+				DPrintf("server %v response to candidate %v \n", serverId, request.CandidateId)
+				if response.Term > rf.currentTerm && !response.VoteGranted {
+					DPrintf("Candidate %v fail, receive bigger term %v\n", rf.me, response.Term)
+					rf.currentTerm = response.Term // change status to follower?
+					rf.changeServerStatus(FOLLOWER)
+					rf.voteFor = -1
+				}
+
+				if response.Term == rf.currentTerm && rf.status == CANDIDATE {
+					if response.VoteGranted {
+						atomic.AddInt32(&grantedVotes, 1)
+						if grantedVotes > int32(len(rf.peers)/2) {
+							// Be a leader
+							rf.changeServerStatus(LEADER)
+							DPrintf("candiadte %v become leader, term %v", rf.me, rf.currentTerm)
+							rf.brotcastHeartbeat()
+						}
+					}
+
+				}
+			}
+		}(serverId)
+	}
+}
+
+func (rf *Raft) brotcastHeartbeat() {
+
+}
---
+++ 6.824/src/raft/raft_tick.go
@@ -0,0 +1,44 @@
+package raft
+
+import "time"
+import "math/rand"
+
+// The ticker go routine starts a new election if this peer hasn't received
+// heartsbeats recently.
+func (rf *Raft) ticker() {
+	for rf.killed() == false {
+
+		// Your code here to check if a leader election should
+		// be started and to randomize sleeping time using
+		// time.Sleep().
+		select {
+		case <-rf.electionTimer.C:
+			{
+				rf.mu.Lock()
+				DPrintf("election timeout tigger. %v follower(term %v)ready to change to candidate", rf.me, rf.currentTerm)
+				rf.changeServerStatus(CANDIDATE)
+				rf.currentTerm += 1
+				rf.electionTimer.Reset(RandomElectionTimeout())
+				rf.startElection()
+				rf.electionTimer.Reset(RandomElectionTimeout())
+				rf.mu.Unlock()
+			}
+		}
+	}
+}
+
+// no mutex lock
+func RandomElectionTimeout() time.Duration {
+	// random 150-300 ms election timeout.
+	randomDuration := time.Duration(rand.Intn(150)+150) * time.Millisecond
+	return randomDuration
+}
+
+// start a new election timer, it sends tick to chan
+// no mutex lock
+func (rf *Raft) Rest(timeout time.Duration) {
+	rf.electionTimer = time.NewTimer(timeout)
+	go func() {
+		<-rf.electionTimer.C
+	}()
+}
